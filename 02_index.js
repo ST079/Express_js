4266e7690bac65f7587e31ca0403c79ee7bbfeec
Sujan Tamang <suzanyba079@gmail.com>
2024-06-20 14:33:01 +0545

    default route paramater

--- 02_index.js
+++ 02_index.js
@@ -6,9 +6,16 @@ app.get("/", (req, res) => {
   res.json({ msg: "Namaste World" });
 });
 
-app.get("/:Name", (req, res) => {
-  const data = req.params.Name;
-  res.json({ msg: `Namaste ${data}` });
+// app.get("/:Name", (req, res) => {
+//   const data = req.params.Name;
+//   res.json({ msg: `Namaste ${data}` });
+// });
+
+//sending Default value of b
+app.get("/:a/:b?", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b) || Number("5");
+  res.json(`the sum of ${a} + ${b} is ${a + b}`);
 });
 
 app.listen(8000, () => console.log("application is running"));
